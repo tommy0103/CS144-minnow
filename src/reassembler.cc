08f4e02e897061d67285b219c6f986bc6e5d412f
tommy0103 <vast_starry_sky@qq.com>
2025-04-03 04:43:18 +0800

    check1 reassembler finished

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -3,15 +3,112 @@
 
 using namespace std;
 
+template<typename Map>
+Map::iterator insertMap(uint64_t idx, std::string& d, Map& mp) {
+  auto it = mp.find(idx);
+  if(it != mp.end()) {
+    if(it->second.size() < d.size()) {
+      it->second = d;
+    }
+  }
+  else {
+    auto [it_, _] = mp.insert({idx, d});
+    it = it_;
+  }
+  return it;
+}
+
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
-  debug( "unimplemented insert({}, {}, {}) called", first_index, data, is_last_substring );
+  Writer& w = output_.writer();
+  Reader& r = output_.reader();
+  uint64_t capacity_ = w.available_capacity() + r.bytes_popped();
+  if(capacity_ < first_index) return;
+  uint64_t delta = 0;
+  if(pendingId > first_index) {
+    delta = pendingId - first_index;
+  }
+  if(is_last_substring) {
+    overId = first_index + data.size();
+  }
+  if(data.size() > 0) {
+    if(delta > data.size()) {
+      return;
+    }
+    data = data.substr(delta, data.size());
+    if(first_index + data.size() > capacity_) { // (first_index + data.size().old - delta)
+      data = data.substr(0, data.size() - (first_index + data.size() - capacity_)); 
+    }
+  }
+
+  {// Coalesce data segments to eliminate overlaps
+    uint64_t st = first_index + delta, ed = st + data.size();
+    auto it = insertMap(first_index + delta, data, pendingBytes);
+    
+    auto it1 = it;
+    if(it1 != pendingBytes.begin()) {
+      --it1; // idx < st
+      auto& [idx, d] = *it1;
+      if(idx + d.size() > st) {
+        if(idx + d.size() >= ed) {
+          pendingBytes.erase(it);
+          return;
+        }// idx + d.size() < ed
+        d = d.substr(0, d.size() - (idx + d.size() - st));
+      }
+    }
+    auto it2 = it;
+    ++it2; // idx > st
+    while(it2 != pendingBytes.end()) {
+      auto& [idx, d] = *it2;
+      if(ed > idx) {
+        if(ed >= idx + d.size()) {
+          it2 = pendingBytes.erase(it2);
+        }
+        else {// ed < idx + d.size()
+          auto nidx = ed;
+          auto nd = d.substr(ed - idx, d.size());
+          it2 = pendingBytes.erase(it2);
+          insertMap(nidx, nd, pendingBytes);
+          break;
+        }
+      }
+      else {
+        break;
+      }
+    }
+  }
+  
+  // Check data can be sended.
+  for(auto it = pendingBytes.begin(); it != pendingBytes.end(); ) {
+    auto& [idx, d] = *it;
+    if(idx > pendingId) {
+      break;
+    }
+    if(idx + d.size() == overId) {
+      w.close();
+    }
+    if(idx + d.size() <= pendingId) {
+      it = pendingBytes.erase(it);
+      continue;
+    }
+    // pendingId - idx > d.size();
+    if(idx < pendingId) {
+      d = d.substr(pendingId - idx, d.size());
+    }
+    pendingId += d.size();
+    w.push(d);
+    it = pendingBytes.erase(it);
+  }
 }
 
 // How many bytes are stored in the Reassembler itself?
 // This function is for testing only; don't add extra state to support it.
 uint64_t Reassembler::count_bytes_pending() const
 {
-  debug( "unimplemented count_bytes_pending() called" );
-  return {};
+  uint64_t bytesCount = 0;
+  for(auto& [idx, d]: pendingBytes) {
+    bytesCount += d.size();
+  }
+  return bytesCount;
 }
--- src/reassembler.hh
+++ src/reassembler.hh
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "byte_stream.hh"
+#include <map>
 
 class Reassembler
 {
 public:
   // Construct Reassembler to write into given ByteStream.
-  explicit Reassembler( ByteStream&& output ) : output_( std::move( output ) ) {}
+  explicit Reassembler( ByteStream&& output ) : pendingBytes(), pendingId(0), overId(-1), output_( std::move( output ) ) {}
 
   /*
    * Insert a new substring to be reassembled into a ByteStream.
@@ -42,5 +43,7 @@ public:
   const Writer& writer() const { return output_.writer(); }
 
 private:
+  std::map<uint64_t, std::string> pendingBytes;
+  uint64_t pendingId, overId;
   ByteStream output_;
 };
